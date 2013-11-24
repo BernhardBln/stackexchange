c71a939d93960f54dc6ca078fd149fc8e0b02c2d
Bernhard Streit <Bernhard.Streit@gmail.com>
2013-11-24 23:56:21 +0100

    Fix: Transaktionsmanagement aktiviert

--- dbtest/src/test/java/de/bstreit/java/testprojekte/dbtest/DBTest.java
+++ dbtest/src/test/java/de/bstreit/java/testprojekte/dbtest/DBTest.java
@@ -3,7 +3,9 @@ package de.bstreit.java.testprojekte.dbtest;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 public class DBTest extends AbstractH2TestCase {
 
 	@Test
